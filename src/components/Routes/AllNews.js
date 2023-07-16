64765cc3c8da8c11ad2dcce04fa45bc61a125034
codeHysteria28 <graphicglobal67@gmail.com>
2023-07-16 14:40:49 +0100

    change an URL for AllNews to point to correct endpoint

--- src/components/Routes/AllNews.js
+++ src/components/Routes/AllNews.js
@@ -22,7 +22,7 @@ const News = () => {
         setIsLoading(true);
         axios({
             method: 'get',
-            url: 'http://localhost:80/getNewsAdmin',
+            url: 'https://azureedube1.azurewebsites.net/getNewsAdmin',
             withCredentials: true
         }).then(res => {
             setAllNews(res.data);
